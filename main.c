3a5a5983a7d9270df99dcbbce3565714b7da6234
ammar <a.diarra.agla@gmail.com>
2016-11-04 14:12:06 +0100

    YTYHT

--- main.c
+++ main.c
@@ -5,11 +5,11 @@ int fx_addition (int ,int );
 
 int main()
 {   printf("rentrez vos deux valeurs\n");
-    printf("%d",fx_addition(1,4));
+    printf("%d",fx_addition(1,6));
     return 0;
 }
 
 int fx_addition(int a,int b)
 {
-    return a+b
+    return a+b;
 }
