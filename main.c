9305e819fba90d9d1f60714b6759b73eafd2707a
ammar <a.diarra.agla@gmail.com>
2016-11-04 11:50:17 +0100

    on a changé le programme

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int fx_addition (int ,int );
+
 int main()
-{
-    printf("Hello world!\n");
+{   printf("rentrez vos deux valeurs\n");
+    printf("%d",fx_addition(1,4));
     return 0;
+}
+
+int fx_addition(int a,int b)
+{
+    return a+b
 }
